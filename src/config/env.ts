1caa3c2b80c8dd49b790c5101b45f8f1eedbc6bd
vovanchiklitva <vovanchik.litva@gmail.com>
2026-07-26 14:42:43 +0300

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -47,7 +47,7 @@ export const env = {
   openaiApiKey: required("OPENAI_API_KEY"),
   openaiBaseUrl: optional("OPENAI_BASE_URL"),
   aiChatModel: optional("AI_CHAT_MODEL", "gpt-4o-mini"),
-  aiEmbedModel: optional("AI_EMBED_MODEL", "text-embedding-3-small"),,
+  aiEmbedModel: optional("AI_EMBED_MODEL", "text-embedding-3-small"),
 
   wbApiKey: optional("WB_API_KEY"),
   ozonApiKey: optional("OZON_API_KEY"),
